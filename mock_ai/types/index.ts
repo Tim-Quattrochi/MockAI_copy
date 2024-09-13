6f8c3ff111269d40fa9306429f09462b935c1754
Tim Quattrochi <timq82@gmail.com>
2024-09-12 23:59:51 -0400

    refactor: move types outside of component

--- mock_ai/types/index.ts
+++ mock_ai/types/index.ts
@@ -14,3 +14,17 @@ export type Question = {
   id: number;
   question: string;
 };
+
+export type Result = {
+  question: string;
+  question_id: number;
+  transcript: string;
+  filler_words: string;
+  long_pauses: string;
+  pause_durations: string;
+  interview_date: string;
+  ai_feedback?: string;
+  audio_url?: string;
+  video_url?: string;
+  score?: number;
+};
