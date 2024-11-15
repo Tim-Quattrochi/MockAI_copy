f81ace8a77ba0c219c0499e5eb8240c0dbe0c681
Tim Quattrochi <timq82@gmail.com>
2024-11-15 15:13:44 -0500

    chore: make image priority for lcp

--- apps/mock_ai/components/Header.tsx
+++ apps/mock_ai/components/Header.tsx
@@ -10,9 +10,10 @@ const Header = () => {
     <header className="px-4 lg:px-6  flex items-center">
       <Link className="flex items-center justify-center" href="/">
         <Image
-          className="rounded-lg shadow-lg bg-white z-10 m-1 w-20 h-16 sm:w-24 sm:h-20"
+          className="rounded-lg shadow-lg bg-white z-10 m-1 w-auto h-auto sm:w-24 sm:h-20"
           src="/mockAILogo.jpeg"
           alt="Mockai Logo"
+          priority={true}
           width={80}
           height={65}
         />
