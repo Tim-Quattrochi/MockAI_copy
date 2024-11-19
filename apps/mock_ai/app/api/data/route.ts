a22d2084f1a70328eb5ae8d94866d501d35e3137
Tim Quattrochi <timq82@gmail.com>
2024-11-19 00:09:41 -0500

    refactor: use prisma

--- apps/mock_ai/app/api/data/route.ts
+++ apps/mock_ai/app/api/data/route.ts
@@ -1,13 +1,24 @@
-import { supabase } from "@/lib/supabase";
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const { data, error } = await supabase.from("Question").select("*");
-  if (error) {
-    return NextResponse.json(
-      { error: error.message },
-      { status: 500 }
-    );
+  try {
+    const questions = await prisma.question.findMany();
+
+    return NextResponse.json(questions);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error fetching questions:", error.message);
+      return NextResponse.json(
+        { error: error.message },
+        { status: 500 }
+      );
+    } else {
+      console.error("An unknown error occurred");
+      return NextResponse.json(
+        { error: "An unknown error occurred" },
+        { status: 500 }
+      );
+    }
   }
-  return NextResponse.json(data);
 }
