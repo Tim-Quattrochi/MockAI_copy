4e54227d5bb02b244f9df177bb2fb5fad98dea44
Tim Quattrochi <timq82@gmail.com>
2024-11-29 19:14:42 -0500

    refactor: trying to access tmp file in the same serverless func

--- mock_ai/app/api/audio/transcribe/route.ts
+++ mock_ai/app/api/audio/transcribe/route.ts
@@ -8,7 +8,6 @@ import {
 } from "@google/generative-ai/server";
 import { analyzeAudio } from "@/utils/transcriptionUtils";
 import { generateTranscription } from "@/utils/gemini/genAiUtils";
-import { uploadAndProcessFile } from "@/utils/gemini/fileManagerUtils";
 
 export async function POST(
   request: NextRequest
@@ -33,7 +32,41 @@ export async function POST(
       process.env.GEMINI_API_KEY!
     );
 
-    console.log("VIDEO FILE PATH:", videoFilePath);
+    async function uploadAndProcessFile(
+      fileManager: GoogleAIFileManager,
+      videoFilePath: string,
+      mimeType: string
+    ) {
+      const uploadResult = await fileManager.uploadFile(
+        videoFilePath,
+        {
+          mimeType,
+          displayName: videoFilePath.split("/").pop(),
+        }
+      );
+
+      console.log(
+        "upload result from file manager utils: ",
+        uploadResult
+      );
+
+      let file = await fileManager.getFile(uploadResult.file.name);
+      while (file.state === FileState.PROCESSING) {
+        process.stdout.write(".");
+        await new Promise((resolve) => setTimeout(resolve, 10_000));
+        file = await fileManager.getFile(uploadResult.file.name);
+      }
+
+      console.log(
+        `Uploaded file ${uploadResult.file.displayName} as: ${uploadResult.file.uri}`
+      );
+
+      if (file.state === FileState.FAILED) {
+        throw new Error("Audio processing failed.");
+      }
+
+      return uploadResult.file.uri;
+    }
 
     const fileUri = await uploadAndProcessFile(
       fileManager,
